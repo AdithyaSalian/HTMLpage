a2c90fe4868c165f5fc9c0dc06ccacc50378608c
Adithya Salian <136426751+AdithyaSalian@users.noreply.github.com>
2023-09-15 10:31:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const themeButton = document.getElementById("theme-button");
+    const body = document.body;
+
+   
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-mode");
+    }
+
+   
+    themeButton.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+  
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+});
